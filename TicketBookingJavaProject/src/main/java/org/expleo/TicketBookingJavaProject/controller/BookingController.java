8adf201854dc621eb2095cd5742a65ed6338f448
RohiniM1607 <mrohini7558@gmail.com>
2026-03-19 19:25:24 +0530

    Ticket Booking Module

--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
@@ -1,5 +1,19 @@
 package org.expleo.TicketBookingJavaProject.controller;
 
-public class BookingController {
+import org.expleo.TicketBookingJavaProject.service.BookingService;
 
+public class BookingController {
+	private BookingService service = new BookingService();
+
+    public void checkTicketAvailability(int ticketCount) {
+
+        boolean isAvailable = service.validateTicketCount(ticketCount);
+
+        if (isAvailable) {
+            System.out.println("Tickets available. Proceed to seat selection.");
+        } 
+        else {
+            System.out.println("Not enough seats available.");
+        }
+    }
 }
--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/model/Seat.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/model/Seat.java
@@ -1,5 +1,18 @@
 package org.expleo.TicketBookingJavaProject.model;
 
 public class Seat {
+	private String row;
+    private int number;
+    private String status; 
+
+    public Seat(String row, int number, String status) {
+        this.row = row;
+        this.number = number;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 
 }
