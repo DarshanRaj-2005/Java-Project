5438f5adbde7bb60f7f4d88d8a67ece20af5e60c
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-03-19 13:37:19 +0530

    New maven project created and added classes

---
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/model/Movie.java
@@ -0,0 +1,5 @@
+package org.expleo.TicketBookingJavaProject.model;
+
+public class Movie {
+
+}
---
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/service/MovieService.java
@@ -0,0 +1,5 @@
+package org.expleo.TicketBookingJavaProject.service;
+
+public class MovieService {
+
+}
