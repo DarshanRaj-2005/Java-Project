b5da09fd650c75b0db5841c6b5b4d94b9a99248c
ROHINI M <mrohini7558@gmail.com>
2026-03-20 14:51:50 +0530

    Revert "Ticket Booking Module"

--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
@@ -1,20 +1,5 @@
 package org.expleo.TicketBookingJavaProject.controller;
 
-import org.expleo.TicketBookingJavaProject.service.BookingService;
-
 public class BookingController {
-	private BookingService service = new BookingService();
-	private SeatController seatController = new SeatController();
-    public void checkTicketAvailability(int ticketCount) {
 
-        boolean isAvailable = service.validateTicketCount(ticketCount);
-
-        if (isAvailable) {
-            System.out.println("Tickets available. Proceed to seat selection.");
-            seatController.showSeatSelectionPage();
-        } 
-        else {
-            System.out.println("Not enough seats available.");
-        }
-    }
 }
--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/repository/impl/SeatRepositoryImpl.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/repository/impl/SeatRepositoryImpl.java
@@ -1,40 +1,5 @@
 package org.expleo.TicketBookingJavaProject.repository.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.expleo.TicketBookingJavaProject.model.Seat;
-
 public class SeatRepositoryImpl {
-	public List<Seat> getSeats() {
 
-        List<Seat> seats = new ArrayList<>();
-
-        char[] rows = {'A','B','C','D','E','F','G','H','I','J'};
-
-        for (char row : rows) {
-            for (int num = 1; num <= 10; num++) {
-
-                String status = "AVAILABLE";
-
-                if (
-                    (row == 'A' && num == 5) ||
-                    (row == 'B' && (num == 3 || num == 7)) ||
-                    (row == 'C' && (num == 4 || num == 8)) ||
-                    (row == 'D' && (num == 2 || num == 6)) ||
-                    (row == 'E' && (num == 5 || num == 9)) ||
-                    (row == 'F' && (num == 3 || num == 8)) ||
-                    (row == 'G' && (num == 2 || num == 6)) ||
-                    (row == 'H' && (num == 4 || num == 7)) ||
-                    (row == 'I' && (num == 3 || num == 8)) ||
-                    (row == 'J' && (num == 2 || num == 7))
-                ) {
-                    status = "BOOKED";
-                }
-
-                seats.add(new Seat(String.valueOf(row), num, status));
-            }
-        }
-
-        return seats;
-    }
 }
