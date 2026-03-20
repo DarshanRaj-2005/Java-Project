4c86dff99d8d5e697df81142300fb9ab450f5253
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-03-20 15:25:31 +0530

    Show selection module

--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/service/BookingService.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/service/BookingService.java
@@ -1,5 +1,49 @@
 package org.expleo.TicketBookingJavaProject.service;
 
+import org.expleo.TicketBookingJavaProject.model.*;
+import org.expleo.TicketBookingJavaProject.repository.impl.BookingRepositoryImpl;
+
+import java.util.List;
+
 public class BookingService {
 
+    public List<City> getCities() {
+        return BookingRepositoryImpl.cities;
+    }
+
+    public List<Theatre> getTheatres() {
+        return BookingRepositoryImpl.theatres;
+    }
+
+    public List<Movie> getMovies() {
+        return BookingRepositoryImpl.movies;
+    }
+
+    public List<Showtime> getShowtimes() {
+        return BookingRepositoryImpl.showtimes;
+    }
+
+    public void displayCities(List<City> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i).getName());
+        }
+    }
+
+    public void displayTheatres(List<Theatre> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i).getName());
+        }
+    }
+
+    public void displayMovies(List<Movie> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i).getTitle());
+        }
+    }
+
+    public void displayShowtimes(List<Showtime> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i).getTime());
+        }
+    }
 }
