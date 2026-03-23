b6ebbbdf317c6b5e4f3cb434f735fe4d90075317
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-03-23 18:24:19 +0530

    Modified seat selection module and search module

--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/controller/BookingController.java
@@ -1,78 +1,14 @@
 package org.expleo.TicketBookingJavaProject.controller;
 
-import org.expleo.TicketBookingJavaProject.model.*;
+import org.expleo.TicketBookingJavaProject.model.Seat;
 import org.expleo.TicketBookingJavaProject.service.BookingService;
-import org.expleo.TicketBookingJavaProject.service.MovieService;
 import java.util.*;
 
 public class BookingController {
 
 	private BookingService service = new BookingService();
-	private MovieService service1 = new MovieService();
-	private Scanner sc = new Scanner(System.in);
+
 	private SeatController seatController = new SeatController();
-
-	public void startBookingFlow() {
-
-		System.out.println("Select City:");
-		List<City> cities = service.getCities();
-
-		if (cities.isEmpty()) {
-			System.out.println("No cities available. Admin must add data.");
-			return;
-		}
-
-		service.displayCities(cities);
-		int cityChoice = sc.nextInt();
-
-		System.out.println("\nSelect Theatre:");
-		List<Theatre> theatres = service.getTheatres();
-		service.displayTheatres(theatres);
-		int theatreChoice = sc.nextInt();
-
-		System.out.println("\nSelect Movie:");
-		List<Movie> movies = service.getMovies();
-		service.displayMovies(movies);
-		int movieChoice = sc.nextInt();
-
-		System.out.println("\nSelect Showtime:");
-		List<Showtime> shows = service.getShowtimes();
-		service.displayShowtimes(shows);
-		int showChoice = sc.nextInt();
-
-		System.out.println("\nYou Selected:");
-		System.out.println("City: " + cities.get(cityChoice - 1).getName());
-		System.out.println("Theatre: " + theatres.get(theatreChoice - 1).getName());
-		System.out.println("Movie: " + movies.get(movieChoice - 1).getTitle());
-		System.out.println("Showtime: " + shows.get(showChoice - 1).getTime());
-	}
-
-	public void searchMovie() {
-
-		System.out.println("1. Search by Title");
-		System.out.println("2. Search by Language");
-
-		int choice = sc.nextInt();
-		sc.nextLine();
-
-		if (choice == 1) {
-			System.out.println("Enter title:");
-			String title = sc.nextLine();
-
-			List<Movie> result = service1.searchByTitle(title);
-			service.displayMovies(result);
-
-		} else if (choice == 2) {
-			System.out.println("Enter language:");
-			String lang = sc.nextLine();
-
-			List<Movie> result = service1.searchByLanguage(lang);
-			service.displayMovies(result);
-
-		} else {
-			System.out.println("Invalid choice");
-		}
-	}
 
 	public void checkTicketAvailability(int ticketCount) {
 
--- TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/service/BookingService.java
+++ TicketBookingJavaProject/src/main/java/org/expleo/TicketBookingJavaProject/service/BookingService.java
@@ -3,71 +3,26 @@ package org.expleo.TicketBookingJavaProject.service;
 import java.util.List;
 import org.expleo.TicketBookingJavaProject.model.Seat;
 import org.expleo.TicketBookingJavaProject.repository.impl.SeatRepositoryImpl;
-import org.expleo.TicketBookingJavaProject.model.*;
-import org.expleo.TicketBookingJavaProject.repository.impl.BookingRepositoryImpl;
 
 public class BookingService {
 
 	private SeatRepositoryImpl repo = new SeatRepositoryImpl();
 
-    public boolean validateTicketCount(int ticketCount) {
+	public boolean validateTicketCount(int ticketCount) {
 
-        if (ticketCount <= 0) {
-            return false;
-        }
-
-        List<Seat> seats = repo.getSeats();
-        int availableSeats = 0;
-
-        for (Seat seat : seats) {
-            if (seat.getStatus().equalsIgnoreCase("AVAILABLE")) {
-                availableSeats++;
-            }
-        }
-
-        return ticketCount <= availableSeats;
-    }
-    
-    
-	public List<City> getCities() {
-		return BookingRepositoryImpl.cities;
-	}
-
-	public List<Theatre> getTheatres() {
-		return BookingRepositoryImpl.theatres;
-	}
-
-	public List<Movie> getMovies() {
-		return BookingRepositoryImpl.movies;
-	}
-
-	public List<Showtime> getShowtimes() {
-		return BookingRepositoryImpl.showtimes;
-	}
-
-	public void displayCities(List<City> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println((i + 1) + ". " + list.get(i).getName());
+		if (ticketCount <= 0) {
+			return false;
 		}
-	}
 
-	public void displayTheatres(List<Theatre> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println((i + 1) + ". " + list.get(i).getName());
-		}
-	}
+		List<Seat> seats = repo.getSeats();
+		int availableSeats = 0;
 
-	public void displayMovies(List<Movie> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println((i + 1) + ". " + list.get(i).getTitle());
+		for (Seat seat : seats) {
+			if (seat.getStatus().equalsIgnoreCase("AVAILABLE")) {
+				availableSeats++;
+			}
 		}
-	}
 
-	public void displayShowtimes(List<Showtime> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println((i + 1) + ". " + list.get(i).getTime());
-		}
+		return ticketCount <= availableSeats;
 	}
-	
-	
 }
